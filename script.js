ddc7cba303b3f9661e1a6cc981351e9c74b4cb11
perezsergioace <perez.sergio.ace@gmail.com>
2021-05-11 22:33:02 -0400

    adding animations for page

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ anime.timeline().add({
     duration: 2000,
     delay: (el, i) => 2000 + 60 * i,
 });
+
+TweenMax.from(".left", 3, {
+    left: "-50%",
+    ease: Expo.easeInOut,
+    delay: 3.4,
+});
+
+TweenMax.from(".header h1", 3, {
+    left: "-140%",
+    ease: Expo.easeInOut,
+    delay: 3.4,
+});
+
+TweenMax.staggerFrom(".images > div", 1, {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 6
+}, 0.2);
+
+TweenMax.staggerFrom(".header > p", 1, {
+    y: "60",
+    opacity: 0,
+    ease: Power2.easeOut,
+    delay: 5.6
+}, 0.2);
+
+TweenMax.from(".link", 1, {
+    opacity: 0,
+    ease: SteppedEase.config(1),
+    repeat: -1,
+    repeatDelay: 0.2,
+    delay: 7.8,
+});
